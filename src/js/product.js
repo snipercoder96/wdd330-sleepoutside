eea17c9ae7b760083d822b561d7bb99ce1b9b5c2
snipercoder96 <kmbembakiyindou@byupathway.edu>
2026-03-06 18:04:16 +0200

    Removed <summary> tag

--- src/js/product.js
+++ src/js/product.js
@@ -5,11 +5,11 @@ const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
 
-  <summary>
-    // 1. The issue is not checking if whether the so-cart is exsiting in localstorage
-    // 2. cHECK if it is an array, if so, set the items to be empty
-    // 3. Push the product selected by the user into the items list.
-  </summary>
+  
+  // 1. The issue is not checking if whether the so-cart is exsiting in localstorage
+  // 2. cHECK if it is an array, if so, set the items to be empty
+  // 3. Push the product selected by the user into the items list.
+  
 
   let items = getLocalStorage("so-cart");
 
