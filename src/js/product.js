7285146eb1d192b061cb98fda37250c7c31c2bfa
snipercoder96 <kmbembakiyindou@byupathway.edu>
2026-03-06 18:01:32 +0200

    Fixed Add to cart issue

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,22 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
+
+  <summary>
+    // 1. The issue is not checking if whether the so-cart is exsiting in localstorage
+    // 2. cHECK if it is an array, if so, set the items to be empty
+    // 3. Push the product selected by the user into the items list.
+  </summary>
+
+  let items = getLocalStorage("so-cart");
+
+  if(!Array.isArray(items)) items = [];
+
+  items.push(product);
+
   setLocalStorage("so-cart", product);
 }
 // add to cart button event handler
