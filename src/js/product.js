0acc1f8268a385dda6cdbcaf023642585461d852
snipercoder96 <kmbembakiyindou@byupathway.edu>
2026-03-06 18:27:09 +0200

    Removed <summary> tag

--- src/js/product.js
+++ src/js/product.js
@@ -4,16 +4,13 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-
-  
   // 1. The issue is not checking if whether the so-cart is exsiting in localstorage
   // 2. cHECK if it is an array, if so, set the items to be empty
   // 3. Push the product selected by the user into the items list.
-  
 
   let items = getLocalStorage("so-cart");
 
-  if(!Array.isArray(items)) items = [];
+  if (!Array.isArray(items)) items = [];
 
   items.push(product);
 
