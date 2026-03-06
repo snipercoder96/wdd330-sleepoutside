42671c8c4177c35a8c788c97fcebe9006334e267
snipercoder96 <kmbembakiyindou@byupathway.edu>
2026-03-06 15:10:12 +0200

    Fixed add to item cart issue

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,13 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const items = getLocalStorage("so-cart") || []; // 1. The fix needed: Set it empty if not found, then add the items
+  // 1. The fix needed: Set it empty if not found, then add the items
+  let items = getLocalStorage("so-cart");
+
+  if (!Array.isArray(items)) {
+    items = [];
+  }
+
   items.push(product);
 
   setLocalStorage("so-cart", items);
