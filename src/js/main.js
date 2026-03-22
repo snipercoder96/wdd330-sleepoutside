92e0434e115fe959f0cacf1c5b2b6034fd18b97e
snipercoder96 <kmbembakiyindou@byupathway.edu>
2026-03-22 21:41:08 +0200

    Made changes to async function

--- src/js/main.js
+++ src/js/main.js
@@ -8,19 +8,25 @@ const dataSource = new ProductData();
 const element = document.querySelector(".product-list");
 const listing = new ProductList("tents", dataSource, element);
 
-await listing.init();
-
 // adding feature search
-document.getElementById("searchInput").addEventListener("input", (e) => {
-  const query = e.target.value.toLowerCase();
-  if (query.length === 0) {
-    // make sure that the query is not empty
-    listing.renderList(listing.products || []);
-    return;
-  }
-  if (query.length < 3) return; // if its less than 3 dont reutrn any resut because there wil be no filtering happening if characters are not at least 3
-  const filtered = (listing.products || []).filter((product) =>
-    product.Name.toLowerCase().includes(query),
-  );
-  listing.renderList(filtered);
-});
+async function init() {
+  await listing.init();
+
+  document.getElementById("searchInput").addEventListener("input", (e) => {
+    const query = e.target.value.toLowerCase();
+    console.log("query:", query);
+    console.log("products:", listing.products);
+    if (query.length === 0) {
+      listing.renderList(listing.products || []);
+      return;
+    }
+    if (query.length < 3) return;
+    const filtered = (listing.products || []).filter((product) =>
+      product.Name.toLowerCase().includes(query)
+    );
+    console.log("filtered:", filtered);
+    listing.renderList(filtered);
+  });
+}
+
+init();
