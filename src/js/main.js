47e01f077dad5c78ba73f269137020dfc5a4119c
snipercoder96 <kmbembakiyindou@byupathway.edu>
2026-03-22 19:51:33 +0200

    Made changes to product_listing

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,4 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.js";
 
 loadHeaderFooter();
 
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const productList = new ProductList("Tents", dataSource, element);
-
-productList.init();
