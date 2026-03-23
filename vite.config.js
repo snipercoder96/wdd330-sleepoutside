cd8a90485232ea171e9c1d23d37c633207d6171c
snipercoder96 <kmbembakiyindou@byupathway.edu>
2026-03-23 16:18:42 +0200

    imported product pages in index.html

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,7 @@ export default defineConfig({
           "src/product_pages/northface-talus-4.html",
         ),
         productListing: resolve(__dirname, "src/product_listing/index.html"),
+        productPages: resolve(__dirname, "src/product_pages/index.html"),
       },
     },
   },
